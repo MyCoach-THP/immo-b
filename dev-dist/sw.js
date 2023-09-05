28fa5d9ad9ddacdde651dd97ac06a485cf3caf26
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2023-09-05 09:48:23 +0200

    worldmap

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.60d3s76nno8"
+    "revision": "0.k2afqf0tuq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
