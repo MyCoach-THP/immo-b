66bae7ae922806df74f68481870d38aa4d65874c
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2023-09-05 10:05:20 +0200

    Worldmap ok

---
+++ src/components/Leaflet/providers.js
@@ -0,0 +1,6 @@
+export default {
+    maptiler:{
+        url : "https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=c2NFJzzCUfp0vvXUZGxQ",
+        attribution: `https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors`
+    }
+}
